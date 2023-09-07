a5810d6657747c36bf0a52125a9753737ac9e5ce
Aditya Raj <101439988+RajAditya01@users.noreply.github.com>
2023-09-07 14:31:03 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,64 @@
-import react from "react";
+// import react from "react";
 
-let count=0;
+// let count=0;
 
-const IncNUM = () => {
-    console.log("clicked");
-};
+// const IncNUM = () => {
+//     count++;
+//     console.log("clicked");
+// };
 
-const App = () =>{
-    return (
-        <>
-        <h1>{count}</h1>
-        <button onClick={IncNUM}>Click Me</button>
-        </>
-    )
-};
-
-export default App;
-
-
-
-
-// import React, { useState } from "react";
-
-// const App = () => {
-//   const [count, setCount] = useState(0);
-
-//   const IncNUM = () => {
-//     setCount(count + 1);
-//   };
-
-//   return (
-//     <>
-//       <h1>{count}</h1>
-//       <button onClick={IncNUM}>Click Me</button>
-//     </>
-//   );
+// const App = () =>{
+//     return (
+//         <>
+//         <h1>{count}</h1>
+//         <button onClick={IncNUM}>Click Me</button>
+//         </>
+//     )
 // };
 
 // export default App;
+
+
+//increment
+// import React, { useState } from "react";
+// const App = () => {
+//     const [count, setCount] = useState(0);
+
+//     const IncNUM = () => {
+//         setCount(count + 1);
+//     };
+
+//     return (
+//         <>
+//             <h1>{count}</h1>
+//             <button onClick={IncNUM}>Click Me</button>
+//         </>
+//     );
+// };
+
+// export default App;
+
+
+//increment + decrement
+import React, { useState } from "react";
+
+const App = () => {
+    const [count, setCount] = useState(0);
+
+    const increment = () => {
+        setCount(count + 1);
+    };
+
+    const decrement = () => {
+        setCount(count - 1);
+    };
+
+    return (<>
+            <h1>{count}</h1>
+            <button onClick={increment}>Increment</button>
+            <button onClick={decrement}>Decrement</button>
+        </>
+    );
+};
+
+export default App;
