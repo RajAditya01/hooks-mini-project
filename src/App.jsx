fc3c50d622489486648cf695b9eaa6014cbb321a
Aditya Raj <101439988+RajAditya01@users.noreply.github.com>
2023-09-18 18:48:33 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -40,23 +40,73 @@
 
 
 //increment + decrement
-import React, { useState } from "react";
+// import React, { useState } from "react";
+
+// const App = () => {
+//     const [count, setCount] = useState(0);
+
+//     const increment = () => {
+//         setCount(count + 1);
+//     };
+
+//     const decrement = () => {
+//         setCount(count - 1);
+//     };
+
+//     return (<>
+//             <h1>{count}</h1>
+//             <button onClick={increment}>Increment</button>
+//             <button onClick={decrement}>Decrement</button>
+//         </>
+//     );
+// };
+
+// export default App;
+
+
+//challenges
+// import React, { useState } from "react";
+
+// const App = () => {
+//     const [currentTime, setCurrentTime] = useState("");
+
+//     const getTime = () => {
+//         const currTime = new Date().toLocaleTimeString();
+//         setCurrentTime(currTime);
+//     };
+
+//     return (
+//         <>
+//             <h1>{currentTime}</h1>
+//             <button onClick={getTime}>GET TIME</button>
+//         </>
+//     );
+// };
+
+// export default App;
+
+
+//creating a digital project
+import React, { useState, useEffect } from "react";
 
 const App = () => {
-    const [count, setCount] = useState(0);
+    // Initialize the state with the current time
+    const [ctime, setCtime] = useState(new Date().toLocaleTimeString());
 
-    const increment = () => {
-        setCount(count + 1);
-    };
+    // Use useEffect to update the time every second
+    useEffect(() => {
+        const interval = setInterval(() => {
+            // Update the time in the state
+            setCtime(new Date().toLocaleTimeString());
+        }, 1000); // Update every 1000 milliseconds (1 second)
 
-    const decrement = () => {
-        setCount(count - 1);
-    };
+        // Cleanup the interval on component unmount
+        return () => clearInterval(interval);
+    }, []);
 
-    return (<>
-            <h1>{count}</h1>
-            <button onClick={increment}>Increment</button>
-            <button onClick={decrement}>Decrement</button>
+    return (
+        <>
+            <h1>{ctime}</h1>
         </>
     );
 };
