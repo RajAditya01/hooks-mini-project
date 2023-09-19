313224c193f1d3bafb0184cd24671fbb53ca1fa7
Aditya Raj <101439988+RajAditya01@users.noreply.github.com>
2023-09-19 17:45:20 +0530

    .

--- src/EventHandling.jsx
+++ src/EventHandling.jsx
@@ -1,10 +1,21 @@
-import react from react;
+import React from 'react'; // Correct import statement
+import { useState } from 'react';
 
 const EventHandling = () => {
-    return(
-        <>
-        <div>
-        <button>Click Me</button>
+    const purple = "#8e44ad";
+    const [bg, setBg] =useState(purple);
+    const [name, setName] = useState("click Me");
+
+    const bgChange = () => {
+        let newBg="#34495e";
+        setBg(newBg);
+        setName("Ouch!! 🐺");
+        //alert('Button clicked!'); // Replace with your desired event handling code
+    };
+
+    return (<>
+        <div style={ {backgroundColor: bg } }>
+            <button onClick={bgChange}>{name}</button> {/* Add onClick event handler */}
         </div>
         </>
     );
